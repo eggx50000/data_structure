8faad51dc2338515d4af699df8dbebd95abfc273
joon <eggx50000@gmail.com>
2021-10-21 22:46:31 +0900

    트라이 첫 번째 구현 입니다.

---
+++ Data/trie.h
@@ -0,0 +1,28 @@
+#pragma once
+
+struct node{
+    node *child[26];
+    bool en, ch;
+    node(){
+        for(int i = 0; i < 26; i ++) child[i] = nullptr;
+        en = false;
+    }
+    ~node(){
+        for(int i = 0; i < 26; i ++){
+            if(child[i] != nullptr) delete child[i];
+        }
+    }
+    void ins(char *st){
+        if(*st == 0){
+            en = true;
+            return;
+        }
+        ch = true;
+        if(child[*st - 'a'] != nullptr) child[*st - 'a'] -> ins(st + 1);
+        else{
+            child[*st - 'a'] = new node;
+            child[*st - 'a'] -> ins(st + 1);
+        }
+    }
+
+};
